2acd8fe6a63c02dcdc24a96bd8274903f1eea3b7
YUNJAEGOONER <coygyj@gmail.com>
2024-09-09 22:28:13 +0900

    ãlesson-7

---
+++ src/main/java/com/example/spring_security_JWT/jwt/LoginFilter.java
@@ -0,0 +1,47 @@
+package com.example.spring_security_JWT.jwt;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+	private final AuthenticationManager authenticationManager;
+
+	public LoginFilter(AuthenticationManager authenticationManager){
+		this.authenticationManager = authenticationManager;
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+		//클라이언트의 요청(request)로부터 username, password 추출
+		//해당 정보를 이용해 인증을 진행
+		String username = obtainUsername(request);
+		String password = obtainPassword(request);
+
+		//System.out.println(username);
+
+		UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password, null);
+
+		return authenticationManager.authenticate(authToken);
+	}
+
+	//로그인 성공 (JWT 발급)
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+
+	}
+
+	//로그인 실패
+	@Override
+	protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+
+	}
+}
